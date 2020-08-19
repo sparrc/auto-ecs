ee07f8745071e468fdd342f4e19248c3a7e8efac
Cam <cameronsparr@gmail.com>
2020-08-19 16:07:14 -0700

    cd to agent dir in zshrc, down.sh fix clustername tag

--- down.sh
+++ down.sh
@@ -8,7 +8,7 @@ if [[ "$CLUSTERNAME" == "" ]]; then
 fi
 
 # find all instances that are part of the cluster:
-for instanceID in $(aws ec2 describe-instances --filters "Name=tag:Cluster,Values=o0c" | jq -r ".Reservations[].Instances[].InstanceId"); do
+for instanceID in $(aws ec2 describe-instances --filters "Name=tag:Cluster,Values=$CLUSTERNAME" | jq -r ".Reservations[].Instances[].InstanceId"); do
     echo "Terminating $instanceID"
     aws ec2 terminate-instances --instance-ids $instanceID | jq .
 done
