ed75db72824c23ef1b2977087a40c507c4a0b688
Cam <gh@sparr.email>
2021-01-05 11:09:25 -0800

    make separate script for adding windows instance

--- add-windows-instance-to-cluster.sh
+++ add-windows-instance-to-cluster.sh
@@ -18,32 +18,19 @@ SUBNETID=$(jq -r .subnet1ID <"./clusters/$CLUSTERNAME.json")
 CLUSTERNAME=$(jq -r .clusterName <"./clusters/$CLUSTERNAME.json")
 REGION=$(jq -r .region <"./clusters/$CLUSTERNAME.json")
 
-AMIID=""
-TYPE_PREFIX="${INSTANCE_TYPE:0:3}"
-case $TYPE_PREFIX in
-a1. | m6g | c6g | r6g | t4g)
-    echo "ARM instance type detected"
-    AMIID=$(aws ssm get-parameters --region "$REGION" --names /aws/service/ecs/optimized-ami/amazon-linux-2/arm64/recommended/image_id | jq -r ".Parameters[0].Value")
-    ;;
-p2. | p3. | p4d | g4d | g3s | g3.)
-    echo "GPU instance type detected"
-    AMIID=$(aws ssm get-parameters --region "$REGION" --names /aws/service/ecs/optimized-ami/amazon-linux-2/gpu/recommended/image_id | jq -r ".Parameters[0].Value")
-    ;;
-*)
-    AMIID=$(aws ssm get-parameters --region "$REGION" --names /aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id | jq -r ".Parameters[0].Value")
-    # AL1 AMI
-    #AMIID=$(aws ssm get-parameters --region "$REGION" --names /aws/service/ecs/optimized-ami/amazon-linux/recommended/image_id | jq -r ".Parameters[0].Value")
-    ;;
-esac
+# Windows AMI and userdata
+AMIID=$(aws ssm get-parameters --names /aws/service/ami-windows-latest/Windows_Server-2019-English-Full-ECS_Optimized/image_id | jq -r ".Parameters[0].Value")
 
-# setup userdata
-cat ./userdata >/tmp/userdata
-cat <<EOF >>/tmp/userdata
-echo ECS_CLUSTER=$CLUSTERNAME >> /etc/ecs/ecs.config
+cat << EOF > /tmp/userdata
+<powershell>
+[Environment]::SetEnvironmentVariable("ECS_ENABLE_SPOT_INSTANCE_DRAINING", "true", "Machine")
+Import-Module ECSTools
+Initialize-ECSAgent -Cluster '$CLUSTERNAME' -EnableTaskIAMRole
+</powershell>
 EOF
 
 # get spot price
-price=$(aws ec2 describe-spot-price-history --instance-type "$INSTANCE_TYPE" --region "$REGION" --product-description "Linux/UNIX" --availability-zone "${REGION}a" | jq -r ".SpotPriceHistory[0].SpotPrice")
+price=$(aws ec2 describe-spot-price-history --instance-type "$INSTANCE_TYPE" --region "$REGION" --product-description "Windows" --availability-zone "${REGION}a" | jq -r ".SpotPriceHistory[0].SpotPrice")
 bid=$(echo "$price * 2" | bc -l)
 echo "Spot price of instance $INSTANCE_TYPE is approximately \$$price/hour, bidding \$$bid/hour"
 
